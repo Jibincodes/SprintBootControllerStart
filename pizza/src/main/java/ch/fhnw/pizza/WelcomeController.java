0be80f70d6b1a0085c94ef043e803ee314730df3
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-04-18 09:38:16 +0000

    - learning mapping for updating, deleting an pizza using mapping

--- pizza/src/main/java/ch/fhnw/pizza/WelcomeController.java
+++ pizza/src/main/java/ch/fhnw/pizza/WelcomeController.java
@@ -1,6 +1,8 @@
 package ch.fhnw.pizza;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,11 +10,11 @@ public class WelcomeController {
 
     @GetMapping(value="welcome") // this is mapped to http://localhost:8080/welcome 
     // so remember to start the application and change url to welcome
-    // if you want it in the base url, you can change it to @GetMapping(value="/")
+     //if you want it in the base url, you can change it to @GetMapping(value="/")
+    //@GetMapping(value="/") // this is mapped to http://localhost:8080/
     public String getWelcomeString() {
         
         return "Hello, welcome to our Pizzeria!";
     }
-
-
+    
 }
