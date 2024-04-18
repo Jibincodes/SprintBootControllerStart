b4979af820fdbe17edd2414b348aae98779866e1
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-04-18 08:36:41 +0000

    - to improve my understanding in welcome controller

--- pizza/src/main/java/ch/fhnw/pizza/WelcomeController.java
+++ pizza/src/main/java/ch/fhnw/pizza/WelcomeController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping(value="welcome")
+    @GetMapping(value="welcome") // this is mapped to http://localhost:8080/welcome 
+    // so remember to start the application and change url to welcome
+    // if you want it in the base url, you can change it to @GetMapping(value="/")
     public String getWelcomeString() {
         
         return "Hello, welcome to our Pizzeria!";
