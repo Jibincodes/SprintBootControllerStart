32eedbd0df3534bd82669b74e3943cbe4b2c1be0
Jibincodes <159591520+Jibincodes@users.noreply.github.com>
2024-04-18 09:36:07 +0000

    -just adding info to learn more

--- pizza/src/main/java/ch/fhnw/pizza/PizzaApplication.java
+++ pizza/src/main/java/ch/fhnw/pizza/PizzaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@RestController if you do not want the welcome controller, you can use this and get the mapping here actually
 public class PizzaApplication {
 
 	public static void main(String[] args) {
